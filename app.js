9ead1e3910bdedaec5c14b3a5699a47a046a07e7
Mike McAllister <mike@logikbox.com>
2020-12-12 10:40:34 -0500

    removed express

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.use(express.static(__dirname + '/src'))
-app.get('/', (req, res) => res.sendFile('index.html'))
-
-app.listen(port, () => console.log('App listening on port ' + port))
